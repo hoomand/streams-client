bb2681349aab54bf1a476f7ac911427008f0b74c
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-04-05 14:02:47 +1100

    feat: add semantic ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamList from "./streams/StreamList";
