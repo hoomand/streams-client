93761170647d486114309534de391a6247527f7b
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-04-07 12:11:49 +1000

    feat(GoogleAuth): add GoogleAuth api and key in env

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+if (!process.env.REACT_APP_GOOGLE_API_KEY) {
+  throw new Error("No API key!");
+}
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: process.env.REACT_APP_GOOGLE_API_KEY,
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange();
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I do not know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,22 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import GoogleAuth from "./GoogleAuth";
 
 class Header extends React.Component {
-  state = {
-    activeItem: "streamer"
-  };
-  handleItemClick = e => {
-    console.log(e);
-  };
   render() {
-    const { activeItem } = this.state;
     return (
-      <Menu secondary>
-        <Menu.Item as={Link} to="/">
-          streamer
-        </Menu.Item>
-        <Menu.Item position="right" as={Link} to="/streams/new">
-          steams
-        </Menu.Item>
-        <Menu.Item
-          position="right"
-          name="login"
-          active={activeItem === "login"}
-          onClick={this.handleItemClick}
-        />
-      </Menu>
+      <div className="ui secondary pointing menu">
+        <Link to="/" className="item">
+          Streamy
+        </Link>
+        <div className="right menu">
+          <Link to="/" className="item">
+            All Streams
+          </Link>
+          <GoogleAuth />
+        </div>
+      </div>
     );
   }
 }
