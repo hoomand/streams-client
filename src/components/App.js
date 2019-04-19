6087dcf05bdf4a89be7153473c779504fdf77c90
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-04-19 11:34:01 +1000

    feat: add custom history and use to navigate

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamList from "./streams/StreamList";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -20,7 +21,7 @@ class App extends React.Component {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
