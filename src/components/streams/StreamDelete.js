03d3352f1f8eb84004c9fc5dc7d522ec3cba7555
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-04-23 08:37:47 +1000

    feat: add delete Modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Modal from "../Modal";
 
 class StreamDelete extends React.Component {
   render() {
-    return <div>Stream Delete</div>;
+    return (
+      <div>
+        Stream Delete
+        <Modal />
+      </div>
+    );
   }
 }
 export default StreamDelete;
